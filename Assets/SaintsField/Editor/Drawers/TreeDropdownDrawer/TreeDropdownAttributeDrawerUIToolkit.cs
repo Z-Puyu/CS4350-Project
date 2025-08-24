e9877d12b6a43d1023b5bb1973227d8cb22b32e9
Clifong <59185915+Clifong@users.noreply.github.com>
2025-08-24 14:04:13 +0800

    Code

---
+++ Assets/SaintsField/Editor/Drawers/TreeDropdownDrawer/TreeDropdownAttributeDrawerUIToolkit.cs
@@ -0,0 +1,52 @@
+#if UNITY_2022_2_OR_NEWER && !SAINTSFIELD_DEBUG_UNITY_BROKEN_FALLBACK && !SAINTSFIELD_UI_TOOLKIT_DISABLE
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using SaintsField.DropdownBase;
+using SaintsField.Editor.Drawers.AdvancedDropdownDrawer;
+using SaintsField.Editor.UIToolkitElements;
+using SaintsField.Editor.Utils;
+using SaintsField.Interfaces;
+using SaintsField.Playa;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SaintsField.Editor.Drawers.TreeDropdownDrawer
+{
+    public partial class TreeDropdownAttributeDrawer
+    {
+        private static string NameButton(SerializedProperty property) => $"{property.propertyPath}__TreeDropdown_Button";
+        private static string NameHelpBox(SerializedProperty property) => $"{property.propertyPath}__TreeDropdown_HelpBox";
+
+        protected override VisualElement CreateFieldUIToolKit(SerializedProperty property,
+            ISaintsAttribute saintsAttribute,
+            IReadOnlyList<PropertyAttribute> allAttributes,
+            VisualElement container,
+            FieldInfo info,
+            object parent)
+        {
+            // AdvancedDropdownMetaInfo initMetaInfo = GetMetaInfo(property, (AdvancedDropdownAttribute)saintsAttribute, info, parent, false);
+            //
+            // UIToolkitUtils.DropdownButtonField dropdownButton = UIToolkitUtils.MakeDropdownButtonUIToolkit(GetPreferredLabel(property));
+            // dropdownButton.style.flexGrow = 1;
+            // dropdownButton.name = NameButton(property);
+            // dropdownButton.userData = initMetaInfo.CurValues;
+            // dropdownButton.ButtonLabelElement.text = GetMetaStackDisplay(initMetaInfo);
+            //
+            // dropdownButton.AddToClassList(ClassAllowDisable);
+            //
+            // EmptyPrefabOverrideElement emptyPrefabOverrideElement = new EmptyPrefabOverrideElement(property);
+            // emptyPrefabOverrideElement.Add(dropdownButton);
+            //
+            // return emptyPrefabOverrideElement;
+
+
+            AdvancedDropdownMetaInfo metaInfo = AdvancedDropdownAttributeDrawer.GetMetaInfo(property, (PathedDropdownAttribute)saintsAttribute, info, parent, false);
+            // Debug.Log(string.Join(",", metaInfo.CurValues));
+            return new SaintsTreeDropdownElement(metaInfo);
+        }
+    }
+}
+#endif
---
+++ Assets/SaintsField/Runtime/AdvancedDropdownAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+// ReSharper disable once CheckNamespace
+namespace SaintsField
+{
+    [Conditional("UNITY_EDITOR")]
+    public class AdvancedDropdownAttribute: PathedDropdownAttribute
+    {
+        public const float DefaultTitleHeight = 45f;
+
+        public const float TitleHeight = DefaultTitleHeight;
+        public const float ItemHeight = -1f;
+        public const float MinHeight = -1f;
+
+        public AdvancedDropdownAttribute(string funcName = null, EUnique unique = EUnique.None) : base(funcName, unique)
+        {
+        }
+
+        public AdvancedDropdownAttribute(EUnique unique) : base(unique)
+        {
+        }
+    }
+}
