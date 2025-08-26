5e5e2414c9caee4b67d85550aadb9883909eaa19
Clifong <59185915+Clifong@users.noreply.github.com>
2025-08-26 22:02:10 +0800

    More stuff

--- Assets/Scripts/Weapon system page/ComponentInfoPanel.cs
+++ Assets/Scripts/Weapon system page/ComponentInfoPanel.cs
@@ -1,13 +1,42 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class ComponentInfoPanel : MonoBehaviour
 {
     public TextMeshProUGUI componentName;
     public TextMeshProUGUI buffDescription;
+    public GameObject buffPanel;
+    public int minHeightWithClosedBuffPanel;
+    public int minHeightWithActiveBuffPanel;
+    private LayoutElement layoutElement;
+
+    void Start()
+    {
+        layoutElement = GetComponent<LayoutElement>();
+        ChangePreferredHeight();
+    }
 
     public void PopulateComponentPanelInfo()
     {
-        
+
+    }
+
+    public void ToggleBuffPanel()
+    {
+        buffPanel.SetActive(!buffPanel.activeSelf);
+        ChangePreferredHeight();
+    }
+
+    private void ChangePreferredHeight()
+    {
+        if (buffPanel.activeSelf)
+        {
+            layoutElement.minHeight = minHeightWithActiveBuffPanel;
+        }
+        else
+        {
+            layoutElement.minHeight = minHeightWithClosedBuffPanel;
+        }
     }
 }
--- Assets/Scripts/Weapon system page/WeaponSystemUIManager.cs
+++ Assets/Scripts/Weapon system page/WeaponSystemUIManager.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 using System.Collections.Generic;
+using SaintsField;
 
 public class WeaponSystemUIManager : MonoBehaviour
 {
     public ComponentInfoPanel componentPanelPrefab;
     private List<GameObject> allSpawnedPanels;
+    private int currentIndex = 0;
+    public SaintsDictionary<int, GameObject> mappedIndexToWeaponDetailPrefab;
 
     public void SpawnComponentInfo()
     {
 
+    }
+
+    public void SetWeaponDetail(int index)
+    {
+        mappedIndexToWeaponDetailPrefab[currentIndex].SetActive(false);
+        currentIndex = index;
+        mappedIndexToWeaponDetailPrefab[currentIndex].SetActive(true);
     }
     
     
