c45466e58e0a1c8a312a7b559f822f4caa085b1a
Puyu <49837577+Z-Puyu@users.noreply.github.com>
2025-09-14 00:27:11 +0800

    Add basic enemy AI

---
+++ Assets/Scripts/Common/GameInstance.cs
@@ -0,0 +1,17 @@
+﻿using DataStructuresForUnity.Runtime.GeneralUtils;
+using Player;
+using SaintsField;
+using UnityEngine;
+
+namespace Common {
+    public sealed class GameInstance : Singleton<GameInstance> {
+        [field: SerializeField, Tag] private string PlayerTag { get; set; } = "Player";
+        
+        public static PlayerController Player { get; private set; }
+
+        protected override void Awake() {
+            base.Awake();
+            GameInstance.Player = GameObject.FindGameObjectWithTag(this.PlayerTag).GetComponent<PlayerController>();
+        }
+    }
+}
---
+++ Assets/Scripts/Game/CharacterControls/Movement.cs
@@ -0,0 +1,39 @@
+﻿using SaintsField;
+using UnityEngine;
+
+namespace Game.CharacterControls {
+    [DisallowMultipleComponent]
+    public abstract class Movement : MonoBehaviour {
+        [field: SerializeField, MinValue(1)] private float Speed { get; set; } = 5;
+        [field: SerializeField, Required] private Transform RootTransform { get; set; }
+        private Vector3 TargetDirection { get; set; }
+        protected Vector3 CurrentVelocity { get; set; }
+        
+        public bool IsMoving => this.TargetDirection != Vector3.zero;
+        
+        public void MoveTo(GameObject target) {
+            this.TargetDirection = (target.transform.position - this.RootTransform.position).normalized;
+        }
+        
+        public void MoveTo(Vector3 location) {
+            this.TargetDirection = (location - this.RootTransform.position).normalized;
+        }
+
+        public void MoveIn(Vector3 direction) {
+            this.TargetDirection = direction.normalized;       
+        }
+        
+        public void Stop() {
+            this.TargetDirection = Vector3.zero;
+        }
+
+        protected virtual void Move() {
+            this.RootTransform.position += this.CurrentVelocity;
+        }
+        
+        protected void FixedUpdate() {
+            this.CurrentVelocity = this.TargetDirection * (this.Speed * Time.fixedDeltaTime);
+            this.Move();
+        }
+    }
+}
