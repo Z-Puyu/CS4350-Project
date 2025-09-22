0b7afe71a0739aed017535bc6b6e09f86e4d6f70
Ma-Yirui <saltiestwonton@gmail.com>
2025-09-23 01:22:36 +0800

    Add rotating ranged weapon sprite

--- Assets/WeaponsSystem/BulletComponents/Piercer.cs
+++ Assets/WeaponsSystem/BulletComponents/Piercer.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 namespace WeaponsSystem.BulletComponents {
     public class Piercer : MonoBehaviour {
-        public int PierceCount { get; private set; }
+        [filed: SerializeField] public int PierceCount { get; private set; }
 
         public void Hit() {
             this.PierceCount -= 1;
-            OnScreenDebugger.Log($"Pierced {this.PierceCount} times");
+            OnScreenDebugger.Log($"Remaining Pierce Count: {this.PierceCount} times");
             if (this.PierceCount <= 0) {
                 this.gameObject.SetActive(false);
             }
---
+++ Assets/WeaponsSystem/RangedWeaponSprite.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+namespace WeaponsSystem {
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class RangedWeaponSprite : MonoBehaviour {
+        [field: SerializeField] private float offset = 0;
+        private SpriteRenderer spriteRenderer;
+        private Camera mainCamera;
+
+        public void Awake() {
+            this.mainCamera = Camera.main;
+            this.transform.position = this.transform.parent.position;
+            this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+        }
+
+        public void Update() {
+            this.transform.position = this.transform.parent.position;
+            this.transform.localScale = new Vector3( Mathf.Sign(this.transform.parent.localScale.x) * Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+            Vector3 mousePosScreen = Input.mousePosition;
+            Vector3 mousePosWorld = this.mainCamera.ScreenToWorldPoint(mousePosScreen);
+            mousePosWorld.z = this.transform.position.z;
+            Vector3 direction = (mousePosWorld - this.transform.parent.position).normalized;
+            
+            this.transform.position += direction * this.offset;
+            
+            Debug.Log(Vector3.SignedAngle(Vector3.right, direction, Vector3.forward));
+            this.transform.rotation = Quaternion.Euler(0, 0, Vector3.SignedAngle(Vector3.right, direction, Vector3.forward));
+            
+            if (Math.Abs(this.transform.rotation.z) > Quaternion.AngleAxis(90, Vector3.forward).z) {
+                this.spriteRenderer.flipY = true;
+            } else {
+                this.spriteRenderer.flipY = false;
+            }
+
+        }
+    }
+}
