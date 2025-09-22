d4488f874be0660791916b2966718e3890d98633
Puyu <49837577+Z-Puyu@users.noreply.github.com>
2025-09-22 23:45:15 +0800

    Fix a bug in enemy AI target detection

--- Assets/Scripts/Game/CharacterControls/Movement2D.cs
+++ Assets/Scripts/Game/CharacterControls/Movement2D.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections.Generic;
+using SaintsField;
 using UnityEngine;
 
 namespace Game.CharacterControls {
+    [RequireComponent(typeof(BoxCollider2D))]
     public class Movement2D : Movement {
         [field: SerializeField] private Rigidbody2D RootRigidBody { get; set; }
+        
+        [field: SerializeField, PropRange(0.01f, 1, 0.01f)] 
+        private float Skin { get; set; } = 0.01f;
+        
+        private BoxCollider2D Collider { get; set; }
+
+        private void Awake() {
+            this.Collider = this.GetComponent<BoxCollider2D>();
+        }
 
         private Vector2 CastUntilBlocked(Vector2 displacement) {
             List<RaycastHit2D> hits = new List<RaycastHit2D>();
-            int hitCount = this.RootRigidBody.Cast(
+            int hitCount = this.Collider.Cast(
                 this.TargetDirection, new ContactFilter2D {
                     useTriggers = false,
                     useLayerMask = true,
                     layerMask = Physics2D.GetLayerCollisionMask(this.gameObject.layer)
-                }, hits, displacement.magnitude
+                }, hits, displacement.magnitude + this.Skin
             );
 
             RaycastHit2D nearest = default;
@@ -26,16 +38,24 @@ namespace Game.CharacterControls {
                 distance = h.distance;
                 nearest = h;
             }
-            
-            return nearest.point - this.RootRigidBody.position;
+
+            if (nearest.collider) {
+                return (distance - this.Skin) * this.TargetDirection;
+            }
+
+            return displacement;
         }
 
         protected override void Move() {
             if (this.RootRigidBody) {
                 Vector2 displacement = this.CurrentVelocity;
+                Vector2 movement = this.CastUntilBlocked(displacement);
+                if (movement == Vector2.zero) {
+                    this.Stop();
+                    return;
+                }
                 
-                
-                this.RootRigidBody.MovePosition(this.RootRigidBody.position + (Vector2)this.CurrentVelocity);
+                this.RootRigidBody.MovePosition(this.RootRigidBody.position + movement);
             } else {
                 base.Move();
             }
