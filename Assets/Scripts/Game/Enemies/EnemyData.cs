cd2e3cdc57c9761a064658da4e2edaceb88271cf
Clifong <59185915+Clifong@users.noreply.github.com>
2025-09-28 11:06:19 +0800

    tweak
    tweak

--- Assets/Scripts/Game/Enemies/EnemyData.cs
+++ Assets/Scripts/Game/Enemies/EnemyData.cs
@@ -8,6 +8,7 @@ namespace Game.Enemies {
         [field: SerializeField] public string Id { get; private set; }
         [field: SerializeField] public string Name { get; private set; }
         [field: SerializeField] public int Experience { get; private set; }
+        
         [field: SerializeField] public AnimatorOverrideController Animations { get; private set; }
         [field: SerializeField] public AttributeTable Attributes { get; private set; }
         [field: SerializeField] public LootTable LootTable { get; private set; }
