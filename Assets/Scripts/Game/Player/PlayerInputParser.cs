9f3c1d13d233c6ce95beac34a7d4e2a9faa5ab5c
Clifong <59185915+Clifong@users.noreply.github.com>
2025-10-02 21:40:33 +0800

    tweak
    tweak

--- Assets/Scripts/Game/Player/PlayerInputParser.cs
+++ Assets/Scripts/Game/Player/PlayerInputParser.cs
@@ -5,6 +5,7 @@ using Game.CharacterControls;
 using GameplayAbilities.Runtime.Abilities;
 using InteractionSystem.Runtime;
 using Inventory_related.Inventory_UI_Manager;
+using Map.Objectives.Objective_UI;
 using ModularItemsAndInventory.Runtime.Inventory;
 using ModularItemsAndInventory.Runtime.Items;
 using SaintsField;
@@ -13,9 +14,11 @@ using UnityEngine.InputSystem;
 using WeaponsSystem;
 using WeaponsSystem.DamageHandling;
 
-namespace Game.Player {
+namespace Game.Player
+{
     [DisallowMultipleComponent]
-    public sealed class PlayerInputParser : MonoBehaviour {
+    public sealed class PlayerInputParser : MonoBehaviour
+    {
         [field: SerializeField, Required] private Interactor Interactor { get; set; }
         [field: SerializeField, Required] private Inventory Inventory { get; set; }
         [field: SerializeField, Required] private InventoryUIManager InventoryUIManager { get; set; }
@@ -24,62 +27,86 @@ namespace Game.Player {
         [field: SerializeField, Required] private Combatant Combatant { get; set; }
         [field: SerializeField, Required] private AbilityRoundRobin AbilityRoundRobin { get; set; }
         [field: SerializeField, Required] private AbilitySystem AbilitySystem { get; set; }
-        
-        public void OnInteract(InputAction.CallbackContext context) {
-            if (!context.performed) {
+        [field: SerializeField, Required] private ObjectiveManager ObjectiveManager { get; set; }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            if (!context.performed)
+            {
                 return;
             }
-            
+
             OnScreenDebugger.Log("Interact");
             this.Interactor.Interact();
         }
-        
-        public void OnToggleInventory(InputAction.CallbackContext context) {
-            if (!context.performed) {
+
+        public void OnToggleInventory(InputAction.CallbackContext context)
+        {
+            if (!context.performed)
+            {
                 return;
             }
-            
+
             // Logging inventory items
             StringBuilder sb = new StringBuilder("Inventory:\n");
-            foreach (KeyValuePair<ItemKey, int> item in this.Inventory) {
+            foreach (KeyValuePair<ItemKey, int> item in this.Inventory)
+            {
                 sb.AppendLine($"{item.Key.Id}: {item.Value}");
             }
-            
+
             OnScreenDebugger.Log(sb.ToString());
-            
+
             bool isInventoryActive = this.InventoryUIManager.gameObject.activeSelf;
-            
+
             // Toggle inventory UI
             this.InventoryUIManager.gameObject.SetActive(!isInventoryActive);
         }
 
-        public void OnAttack(InputAction.CallbackContext context) {
-            if (!context.performed) {
+        public void OnAttack(InputAction.CallbackContext context)
+        {
+            if (!context.performed)
+            {
                 return;
             }
-            
+
             OnScreenDebugger.Log("Attack");
             this.Combatant.StartAttack();
         }
 
-        public void OnMove(InputAction.CallbackContext context) {
-            if (context.performed) {
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
                 Vector2 input = context.ReadValue<Vector2>();
                 this.Movement.MoveIn(input);
-            } else if (context.canceled) {
+            }
+            else if (context.canceled)
+            {
                 this.Movement.Stop();
             }
         }
 
-        public void OnUseWeaponSkill(InputAction.CallbackContext context) {
-            if (context.performed) {
+        public void OnUseWeaponSkill(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
                 this.AbilityRoundRobin.Use(0);
-            } 
+            }
         }
 
-        public void OnUseCharacterSkill(InputAction.CallbackContext context) {
-            if (context.performed) {
+        public void OnUseCharacterSkill(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
                 this.AbilityRoundRobin.Use(1);
+            }
+        }
+
+        public void OnOpenObjective(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                ObjectiveManager.OpenNotebook();
             }
         }
     }
