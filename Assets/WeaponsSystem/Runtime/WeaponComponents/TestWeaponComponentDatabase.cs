a2302a5c35872e20eebd1e7b8569a95a1ed85510
DominicFuMJ <119829202+DominicFuMJ@users.noreply.github.com>
2025-10-16 11:05:55 +0800

    Component Database
    Created a component database, and tested to see that I can access it

---
+++ Assets/WeaponsSystem/Runtime/WeaponComponents/TestWeaponComponentDatabase.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace WeaponsSystem.Runtime.WeaponComponents {
+    public class TestWeaponComponentDatabase : MonoBehaviour {
+        void Start() {
+            Debug.Log("Testing ComponentDatabase...");
+
+            // Force load
+            var allComponents = ComponentDatabase.All;
+
+            foreach (var comp in allComponents) {
+                Debug.Log($"Loaded Component: {comp.itemName} | Price: {comp.price} | Rarity: {comp.rarity}");
+                foreach(var mat in comp.craftingMaterials)
+                {
+                    Debug.Log($"Material cost: {mat.material} | Amount: {mat.amount}");
+                }
+            }
+
+            // Example: Try to get one directly by name
+            if (ComponentDatabase.TryGet("FireComponent", out var fireComp)) {
+                Debug.Log($"Successfully retrieved FireComponent! Name: {fireComp.itemName}");
+            } else {
+                Debug.LogWarning("Could not find FireComponent!");
+            }
+        }
+    }
+}
--- Assets/WeaponsSystem/Runtime/WeaponComponents/WeaponComponent.cs
+++ Assets/WeaponsSystem/Runtime/WeaponComponents/WeaponComponent.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using DataStructuresForUnity.Runtime.Utilities;
 using GameplayAbilities.Runtime.Attributes;
 using GameplayEffects.Runtime;
+using ModularItemsAndInventory.Runtime.Items;
 using Projectiles.Runtime;
 using SaintsField;
 using UnityEngine;
@@ -10,6 +11,18 @@ using WeaponsSystem.Runtime.Weapons;
 namespace WeaponsSystem.Runtime.WeaponComponents {
     [CreateAssetMenu(fileName = "New Weapon Component", menuName = "Weapons/Components/Weapon Component")]
     public class WeaponComponent : ScriptableObject {
+        [SerializeField] public string Id;
+        [Header("Shop Info")]
+        [SerializeField] public string itemName;
+        [SerializeField] public string Description;
+
+        [SerializeField] public Sprite icon;
+        [SerializeField] public int price;
+        [SerializeField] public List<MaterialCost> craftingMaterials = new List<MaterialCost>();
+        [SerializeField] public string rarity = "Common";
+        [SerializeField] public int levelRequirement = 1;
+        [SerializeField] public string category { get; private set; }
+
         [field: SerializeField] List<ProjectileShooterMode> ProjectileModesOnAttack { get; set; } = new List<ProjectileShooterMode>();
         [field: SerializeField] ProjectileShooterMode ProjectileModeOnWeapon { get; set; } = ProjectileShooterMode.Default;
         [field: SerializeReference] private IEffect<IDataReader<string, int>, AttributeSet> EffectOnWeapon { get; set; }
@@ -20,14 +33,23 @@ namespace WeaponsSystem.Runtime.WeaponComponents {
         
         private IRunnableEffect EffectOnWeaponInstance { get; set; }
         private List<IRunnableEffect> EffectsOnAttackInstance { get; set; } = new List<IRunnableEffect>();
-        
-        public void Enable(Weapon weapon, AttributeSet stats) {
-            if (this.EffectOnWeapon == null) {
+
+        public void Enable(Weapon weapon, AttributeSet stats)
+        {
+            if (this.EffectOnWeapon == null)
+            {
                 return;
             }
-            
+
             this.EffectOnWeaponInstance = this.EffectOnWeapon.Apply(stats, stats);
             this.EffectOnWeaponInstance.Start();
+        }
+        
+        [System.Serializable]
+        public struct MaterialCost
+        {
+            public ItemData material;
+            public int amount;
         }
 
         public void Disable(Weapon weapon, AttributeSet stats) {
