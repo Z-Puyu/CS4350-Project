8fbd07a266b37737f0b919072c2360a6a6f867be
DominicFuMJ <119829202+DominicFuMJ@users.noreply.github.com>
2025-10-17 09:30:54 +0800

    Update the blacksmith prefab

---
+++ Assets/Scripts/Weapon system page/ScrollListPopulateComponent.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using WeaponsSystem.Runtime.WeaponComponents;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Weapon_system_page {
+    public class ScrollListPopulateComponent : MonoBehaviour
+    {
+        public GameObject itemPrefab;        // prefab to instantiate
+        public Transform contentParent;       // the �Content� transform under ScrollView
+        void Start()
+        {
+            this.PopulateList();
+        }
+
+        void PopulateList()
+        {
+            var weaponComponents = ComponentDatabase.All;
+            foreach (var data in weaponComponents)
+            {
+                GameObject go = Object.Instantiate(this.itemPrefab, this.contentParent);
+                go.transform.Find("NameText")?.GetComponent<TextMeshProUGUI>().SetText(data.itemName);
+                go.transform.Find("PriceText")?.GetComponent<TextMeshProUGUI>().SetText($"${data.price}");
+                go.transform.Find("RarityText")?.GetComponent<TextMeshProUGUI>().SetText(data.rarity.ToString());
+                var icon = go.transform.Find("Icon")?.GetComponent<Image>();
+                if (icon != null && data.icon != null)
+                    icon.sprite = data.icon;
+                
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
